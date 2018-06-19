126b16df77b91e76e0aea59aee2ebef58e7435d9
xido <erivery2@gmail.com>
2018-06-19 14:47:23 +0900

    slidebanner_2

--- exercise/b_step_02_jquery/js/src/slide_banner_01.js
+++ exercise/b_step_02_jquery/js/src/slide_banner_01.js
@@ -21,18 +21,29 @@
   // banLi 클릭(이벤트)하면,
   banLi.click(function(event){
     event.preventDefault();
+    
     // 해당 요소(선택된 요소)에 클래스이름을 부여('active')
     $(this).addClass('active');
     // 선택된 요소의 다른 형제 요소에는 클래스 이름을 제거
     $(this).siblings('li').removeClass('active');
-    //  ====
+    //  ==== 1차
+   /*
     var i = $(this).index();
     console.log(i); //검증:순서값 파악
     // 광고배너의 클래스 이름을 제거
     bannerIn.removeClass('active');
     // 선택요소와 동일한 순서의 광고배너 클래스이름을 부여('active')
     bannerIn.eq(i).addClass('active');
+    */
+    
+    // ==== 2차
+    // .banner_wrap을 css로 margin-left값을 0, -100%, -200% 값으로 이동처리(각각 테스트)
+      // bannerWrap.css({marginLeft:'-100%'});
+    // .banner_wrap에서 css의 margin-left값을 indicator의 순서값의 -100%만큼 곱한 값으로 처리
+    var i = $(this).index();
+    var n = i * -100;
+    var per = n + '%';
+    // bannerWrap.css({marginLeft:per});
+    bannerWrap.animate({marginLeft:per});
   });
-
-
 })(jQuery);
