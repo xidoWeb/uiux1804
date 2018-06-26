46cbd2b978e27f52fb39c6d9460e5481b3fe8343
xido <erivery2@gmail.com>
2018-06-26 11:51:08 +0900

    jQuery scrollTop2

--- exercise/b_step_02_jquery/js/src/jquery_scroll_01.js
+++ exercise/b_step_02_jquery/js/src/jquery_scroll_01.js
@@ -3,7 +3,7 @@
 
   var topBtn = $('.top_btn');
 
-  //브라우저 스크롤 기능
+  //브라우저 스크롤 기능으로 top버튼 나타나게
   $(window).on('scroll',function(e) {
     var thisTop = $(this).scrollTop();
     console.log(thisTop);
@@ -13,8 +13,19 @@
     }else{
       topBtn.stop().fadeIn();
     }
-
-
   });
 
+  // top버튼 클릭해서 상단으로 이동
+  topBtn.on('click',function(e){
+    e.preventDefault();
+    // $(window).scrollTop(0);  // 한번에 움직이는 단점이 존재
+    $('html, body').animate({scrollTop:0}, 500, 'easeInBounce');
+  });
+
+
+
+
 })(jQuery);
+
+
+
