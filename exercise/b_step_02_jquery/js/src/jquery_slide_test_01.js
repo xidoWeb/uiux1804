48bdf2392fce04612887f9c97d9ba4cfe3980980
xido <erivery2@gmail.com>
2018-06-26 16:35:55 +0900

    slide test 접근성

--- exercise/b_step_02_jquery/js/src/jquery_slide_test_01.js
+++ exercise/b_step_02_jquery/js/src/jquery_slide_test_01.js
@@ -1,10 +1,15 @@
 // jquery_slide_test_01.js
 (function($) {
   $('.product').wrap('<div class="product_wrap"></div>');
-  $('.product_wrap').css({width:'100%', height:'100%'});
-
+  $('.product_wrap').css({width:'100%', height:'100%', overflow:'hidden'});
 
   var product = $('.product');
+  var thisIndex = 0;
+
+  // 접근성 고려1
+  // product영역에서 일부 키보드 탭이 반응하지 않도록 처리!
+  product.find('a').attr({tabIndex:-1});
+  product.children('li').eq(thisIndex).children('a').attr({tabIndex:0});
 
   // indicator클릭시 해당 버튼 색칠
   var indi = $('.indicator').children('li');
@@ -15,11 +20,12 @@
     _this.addClass('active');
     _this.siblings('li').removeClass('active');
 
-    var thisIndex = _this.index();
+    thisIndex = _this.index();
     var i = thisIndex * -100 + '%';
     product.stop().animate({marginLeft:i});
+
+    product.find('a').attr({tabIndex:-1});
+    product.children('li').eq(thisIndex).children('a').attr({tabIndex:0});
   });
-
-
 
 })(jQuery);
