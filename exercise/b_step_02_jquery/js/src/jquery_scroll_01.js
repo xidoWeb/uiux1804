a11b24221893cbc758038e036a5a2897ed2c2ed2
xido <erivery2@gmail.com>
2018-06-26 12:18:31 +0900

    jQuery scrollTop3

--- exercise/b_step_02_jquery/js/src/jquery_scroll_01.js
+++ exercise/b_step_02_jquery/js/src/jquery_scroll_01.js
@@ -19,11 +19,31 @@
   topBtn.on('click',function(e){
     e.preventDefault();
     // $(window).scrollTop(0);  // 한번에 움직이는 단점이 존재
-    $('html, body').animate({scrollTop:0}, 500, 'easeInBounce');
+    $('html, body').animate({scrollTop:0}, 500);
   });
 
 
+// navigation 영역 클릭시 위치 이동
+var headBox = $('#headBox');
+var headUl = headBox.children('ul');
+var headList = headUl.children('li');
 
+// li를 클릭해서 
+headList.on('click',function(e) {
+  // li내부 a가 가지는 기본 이벤트(이동) 제거
+  e.preventDefault();
+
+// 검증(선택된 요소자식(a)의 속성(href) 값 파악)
+  //console.log( $(this).children('a').attr('href') );
+
+// 검증된 속성값을 선택자로 변경
+  var thisLink =    $(this).children('a').attr('href');
+  // 선택자의 위치가 상단에서 얼만큼 떨어져있는지 파악
+  var thisOffset = $(thisLink).offset().top;
+  // console.log(thisOffset);
+
+  $('html, body').animate({scrollTop:thisOffset}, 500);
+});
 
 })(jQuery);
 
