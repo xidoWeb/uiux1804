8f390176b60b6b32bf096deb7e98c49310c771db
xido <erivery2@gmail.com>
2018-06-26 15:45:59 +0900

    slide test basic set

--- exercise/b_step_02_jquery/js/src/jquery_scroll_01.js
+++ exercise/b_step_02_jquery/js/src/jquery_scroll_01.js
@@ -46,6 +46,3 @@ headList.on('click',function(e) {
 });
 
 })(jQuery);
-
-
-
---
+++ exercise/b_step_02_jquery/js/src/jquery_slide_test_01.js
@@ -0,0 +1,8 @@
+// jquery_slide_test_01.js
+(function($) {
+  $('.product').wrap('<div class="product_wrap"></div>');
+  $('.product_wrap').css({width:'100%', height:'100%'});
+
+  
+  
+})(jQuery);
