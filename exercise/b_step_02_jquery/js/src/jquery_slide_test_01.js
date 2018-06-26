747898e7aea495ad59de945d71b1df52e1e626f4
xido <erivery2@gmail.com>
2018-06-26 16:15:00 +0900

    slide test check

--- exercise/b_step_02_jquery/js/src/jquery_slide_test_01.js
+++ exercise/b_step_02_jquery/js/src/jquery_slide_test_01.js
@@ -3,6 +3,23 @@
   $('.product').wrap('<div class="product_wrap"></div>');
   $('.product_wrap').css({width:'100%', height:'100%'});
 
-  
-  
+
+  var product = $('.product');
+
+  // indicator클릭시 해당 버튼 색칠
+  var indi = $('.indicator').children('li');
+  indi.on('click',function(e) {
+    e.preventDefault();
+
+    var _this = $(this);
+    _this.addClass('active');
+    _this.siblings('li').removeClass('active');
+
+    var thisIndex = _this.index();
+    var i = thisIndex * -100 + '%';
+    product.stop().animate({marginLeft:i});
+  });
+
+
+
 })(jQuery);
