c8850edb7e8841c7f7330dd2ac5057d94c1e7bf6
xido <erivery2@gmail.com>
2018-07-02 11:53:04 +0900

    gnb mouseenter show step2

--- exercise/b_step_02_jquery/js/src/jquery_gnb_01.js
+++ exercise/b_step_02_jquery/js/src/jquery_gnb_01.js
@@ -1,13 +1,34 @@
 // jquery_gnb_01.js
-
+(function($) {
 
 // #headBox영역에 마우스 올라가면 gnb하위 메뉴가 나타나게 만들기
+// 1. addClass/removeClass
 
 var headBox = $('#headBox');
-headBox
-.on('mouseenter',function(){
-  $(this).addClass('view');
-})
-.on('mouseleave',function(){
-  $(this).removeClass('view');
-})
+
+// headBox
+//       .on('mouseenter',function(){ $(this).addClass('view'); })
+//       .on('mouseleave',function(){ $(this).removeClass('view'); });
+
+// 2. slideUp/slideDown을 이용
+headBox.find('ol').hide();
+headBox.on('mouseenter',function() {
+          $(this).find('ol').slideDown();
+        })
+        .on('mouseleave',function() {
+          $(this).find('ol').slideUp();
+        });
+
+headBox.find('#gnb')
+       .children('ul')
+       .children('li')
+       .children('a').on('focus',function() {
+         headBox.find('ol').slideDown();
+        });
+
+headBox.find('a').on('blur',function() {
+          headBox.find('ol').slideUp();
+        });
+
+
+})(jQuery);
