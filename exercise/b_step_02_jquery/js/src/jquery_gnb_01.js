5943edc24a90f1032eadd27e6fb1fc8d9dec3c37
xido <erivery2@gmail.com>
2018-07-02 12:49:14 +0900

    gnb mouseenter show step3

--- exercise/b_step_02_jquery/js/src/jquery_gnb_01.js
+++ exercise/b_step_02_jquery/js/src/jquery_gnb_01.js
@@ -10,25 +10,39 @@ var headBox = $('#headBox');
 //       .on('mouseenter',function(){ $(this).addClass('view'); })
 //       .on('mouseleave',function(){ $(this).removeClass('view'); });
 
+// 3. 배경영역 생성
+var nav = $('#gnb');
+nav.after('<div class="nav_bg"></div>');
+var navBg = $('.nav_bg');
+navBg.css({marginLeft:'-15px', position:'absolute', top:'6.25rem', width: 'inherit', height:'15rem',
+          backgroundColor: 'rgba(0,255,100,0.5)', display:'none'});
+
 // 2. slideUp/slideDown을 이용
 headBox.find('ol').hide();
 headBox.on('mouseenter',function() {
-          $(this).find('ol').slideDown();
+          $(this).find('ol').stop().slideDown();
+          navBg.stop().slideDown();
         })
         .on('mouseleave',function() {
-          $(this).find('ol').slideUp();
+          $(this).find('ol').stop().slideUp();
+          navBg.stop().slideUp();
         });
 
 headBox.find('#gnb')
        .children('ul')
        .children('li')
        .children('a').on('focus',function() {
-         headBox.find('ol').slideDown();
+         headBox.find('ol').stop().slideDown();
+         navBg.stop().slideDown();
         });
 
-headBox.find('a').on('blur',function() {
-          headBox.find('ol').slideUp();
-        });
+headBox.find('ol').find('li').eq(-1)
+       .find('a').on('blur',function() {
+         headBox.find('ol').stop().slideUp();
+         navBg.stop().slideUp();
+       });
+
+
 
 
 })(jQuery);
