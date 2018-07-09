354bd86a584ef97adea83d87b3ae4e947c75d55b
xido <erivery2@gmail.com>
2018-07-09 15:46:26 +0900

    form test03 layout js

--- exercise/b_step_01_layout/js/src/form_test_03.js
+++ exercise/b_step_01_layout/js/src/form_test_03.js
@@ -1,4 +1,25 @@
 // form_test_03.js
 (function($) {
-  
+  // .show_btn 클릭해서, form영역을 보이고/사라지게 만들기
+
+var btn = $('.show_btn');
+var form = $('#member');
+
+btn.on('click',function(e){
+  e.preventDefault();
+
+
+  form.stop().slideToggle(500, function(){
+    var formView = $(this).css('display');
+    if(formView === 'none'){
+      btn.find('button').text('열기');
+      btn.find('button').addClass('close');
+    }else{
+      btn.find('button').text('닫기');
+      btn.find('button').removeClass('close');
+    }
+  });
+});
+
+
 })(jQuery);
