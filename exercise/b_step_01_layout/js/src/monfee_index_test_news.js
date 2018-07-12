012f9d5afe7011a9d1be52fd5997fede5b90b20e
xido <erivery2@gmail.com>
2018-07-12 11:42:09 +0900

    monfee news02 type2 innerHtml

--- exercise/b_step_01_layout/js/src/monfee_index_test_news.js
+++ exercise/b_step_01_layout/js/src/monfee_index_test_news.js
@@ -21,12 +21,26 @@ var img  = ['menu_01.png','menu_02.png','menu_03.png','menu_04.png','menu_05.png
 
 var best = $('#bestNews');
 var bestUl = best.find('ul');
-
+ //html복제해서 처리하는 방법
 var i=0; 
-for(; i < img.length-1; i+=1){
+for(; i < img.length; i+=1){
   var bestLi = bestUl.find('li').eq(0).clone();
   bestUl.append(bestLi);
   bestUl.children('li').eq(i).find('.news_img').css({backgroundImage:'url('+ addr + img[i] +')'});
 }
+  bestUl.children('li').eq(img.length).remove();
+
+
+
+/* // 생성해서 처리한다면???
+var i = 0;
+for(; i<img.length; i+=1){
+  bestUl.append('<li>  <a href="#">  <div class="news_img"></div>  <dl>  <dt>???</dt>  <dd>=======</dd>  </dl>  </a>  </li>');
+  bestUl.children('li').eq(i).find('.news_img').css({backgroundImage:'url('+ addr + img[i] +')'});
+}
+bestUl.append('<li></li>');
+bestUl.find('li').eq(-1).css({backgroundImage:'url('+ addr+ 'default_menu.png' +')', backgroundRepeat:'no-repeat'});
+*/
+
 
 })(jQuery);
