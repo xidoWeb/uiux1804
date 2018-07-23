68dcd6533aba9ee645700ee1cc0c3de54f49c84c
xido <erivery2@gmail.com>
2018-07-23 14:24:35 +0900

    modal modify 02

--- exercise/b_step_02_jquery/js/src/gallery_modal_02.js
+++ exercise/b_step_02_jquery/js/src/gallery_modal_02.js
@@ -7,25 +7,29 @@
   mdBg.hide();
 
   var addr   = "../img/gallery/";
-  var useImg = [
-    {thum:'j01.jpg', big:'j01_big.jpg'},
-    {thum:'j02.jpg', big:'j02_big.jpg'},
-    {thum:'j03.jpg', big:'j03_big.jpg'},
-    {thum:'j04.jpg', big:'j04_big.jpg'},
-    {thum:'j05.jpg', big:'j05_big.jpg'},
-    {thum:'j06.jpg', big:'j06_big.jpg'},
-    {thum:'j07.jpg', big:'j07_big.jpg'},
-    {thum:'j08.jpg', big:'j08_big.jpg'},
-    {thum:'j09.jpg', big:'j09_big.jpg'},
-    {thum:'j10.jpg', big:'j10_big.jpg'}
-  ];
+  // var useImg = [
+  //   {thum:'j01.jpg', big:'j01_big.jpg'},
+  //   {thum:'j02.jpg', big:'j02_big.jpg'},
+  //   {thum:'j03.jpg', big:'j03_big.jpg'},
+  //   {thum:'j04.jpg', big:'j04_big.jpg'},
+  //   {thum:'j05.jpg', big:'j05_big.jpg'},
+  //   {thum:'j06.jpg', big:'j06_big.jpg'},
+  //   {thum:'j07.jpg', big:'j07_big.jpg'},
+  //   {thum:'j08.jpg', big:'j08_big.jpg'},
+  //   {thum:'j09.jpg', big:'j09_big.jpg'},
+  //   {thum:'j10.jpg', big:'j10_big.jpg'}
+  // ];
 
-
+var useImg = [];
  // 일정한 순서가 있는 값의 경우는 반복문으로 처리할 수 있다.
- // for(var t=1; t<=10; t+=1){
- //   useImg[t].thum = 'j'+t+'.jpg';  
- //   useImg[t].big  = 'j'+t+'_big.jpg'; 
- //  }
+ for(var t=0; t<10; t+=1){
+   useImg[t]={};
+   var at = t+1;
+   (at < 10) ? at = '0'+ at : at = at;
+   useImg[t].thum = 'j'+at+'.jpg';  
+   useImg[t].big  = 'j'+at+'_big.jpg'; 
+  }
+// console.log(useImg);
 
  for(var i=0; i<list.length; i+=1){
   list.eq(i).children('a').css({backgroundImage:'url(' +  addr + useImg[i].thum + ')'});
@@ -39,12 +43,17 @@ list.on('click', ['a'], function(e) {
   mdBox.css({backgroundImage:'url('+ addr + useImg[thisI].big +')'});
   mdBox.prepend('<div class="close"><button type="button">닫기</button></div>');
 
-  $('.close').css({position:'absolute', top:0, right:0,padding:'1rem', backgroundColor:'#fff'});
+  $('.close').css({position:'absolute', top:0, right:0,padding:'1rem', 
+                   backgroundColor:'#fff'});
 
   mdBox.fadeIn();
   mdBg.fadeIn();
 
   close = $('.close'); 
+  close.on('click',function() { 
+    mdBox.fadeOut( function(){  $(this).children('.close').remove(); });
+    mdBg.fadeOut();
+  }); 
 });
 
 mdBg.on('click',function() { 
@@ -54,10 +63,7 @@ mdBg.on('click',function() {
   mdBg.fadeOut();
 }); 
 
-close.on('click',function() { 
-  mdBox.fadeOut( function(){  $(this).children('.close').remove(); });
-  mdBg.fadeOut();
-}); 
+
 
 
 })(jQuery);
