04d9ecdd2ae505756a084b40a95d3d16df1a614f
xido <erivery2@gmail.com>
2018-08-06 16:57:36 +0900

    fade slide2

--- exercise/b_step_02_jquery/js/src/fadeSlide_02.js
+++ exercise/b_step_02_jquery/js/src/fadeSlide_02.js
@@ -2,30 +2,47 @@
 (function($) {
   var bWrap     = $('.banner_wrap');
   var banner    = bWrap.children('div');
-  var bannerLen = banner.length;
+  var bannerLen = banner.length-1;
   var btn = $('.btn').children('button');
+  var indi = $('.indicator');
+  var indiLi = indi.find('li');
+  var j=0;
 
   var ReZindex = function() {
-    banner    = bWrap.children('div');
-    var i = 1;
+    var i = 0;
     for(; i <= bannerLen; i+=1){
-      var j = i * 10;
+      var j = i * 10 + 10;
       banner.eq(bannerLen-i).css({zIndex:j});
     }
-    banner.stop().fadeIn();
   };
   ReZindex();
-
+   
+  var IndiActive = function(j) {
+    indiLi.eq(j).addClass('active');
+    indiLi.eq(j).siblings('li').removeClass('active');
+    banner.eq(j).fadeIn();
+    banner.eq(j).nextAll().fadeIn();
+    banner.eq(j).prevAll().fadeOut();
+  };
+  IndiActive(j);
+ 
 // 이전/다음 버튼 클릭시 내용보이기처리
   btn.on('click',function(e) {
     e.preventDefault();
     var next = $(this)[0] === $('.next_btn')[0];
-
-    if(next){
-
+    if(next){  
+      ( j >= bannerLen ) ? j = 0 :  j+=1;   
     }else{
-
+      (j <= 0) ?  j=bannerLen : j-=1;
     }
+    IndiActive(j);
   });
+
+// indiLi클릭시 fade배너 위치가 나타나거나 사라지게
+indiLi.on('click',function(e) {
+  e.preventDefault();
+  j = $(this).index();
+  IndiActive(j);
+});
 
 })(jQuery);
